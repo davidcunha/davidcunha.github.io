d0d0c52abe28547b77b363177a63b65b526084ab
David Cunha <davidgoncalvescunha@gmail.com>
2018-12-16 00:10:12 +0000

    fix: env variable naming for deploy in netlify

--- src/components/IGFeed.js
+++ src/components/IGFeed.js
@@ -5,8 +5,8 @@ class IGFeed extends React.Component {
   componentDidMount() {
     const userFeed = new Instafeed({
       get: 'user',
-      userId: process.env.IG_USER_ID,
-      accessToken: process.env.IG_ACCESS_TOKEN,
+      userId: process.env.GATSBY_IG_USER_ID,
+      accessToken: process.env.GATSBY_IG_ACCESS_TOKEN,
       resolution: 'standard_resolution',
       template: `<div class="post">
           <a href="{{link}}" target="_blank" id="{{id}}">
